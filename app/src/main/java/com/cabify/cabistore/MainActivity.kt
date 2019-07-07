51af9ea55009369fae268c2aab926e5338774b24
Matej <matej.vancik@gmail.com>
2019-07-07 19:04:28 +0100

    Initial commit

---
+++ app/src/main/java/com/cabify/cabistore/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.cabify.cabistore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
